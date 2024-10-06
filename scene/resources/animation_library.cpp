73202937bdbec9538bc1c4996be472777c7392cd
Ainsley Su <vhtmscyo@gmail.com>
2024-10-06 21:57:58 +0200

    Use vformat instead of raw string
    Separate commit because this is irrelavent of the test case, but an
    small fix for commit b3162e270e.

--- scene/resources/animation_library.cpp
+++ scene/resources/animation_library.cpp
@@ -50,7 +50,8 @@ String AnimationLibrary::validate_library_name(const String &p_name) {
 }
 
 Error AnimationLibrary::add_animation(const StringName &p_name, const Ref<Animation> &p_animation) {
-	ERR_FAIL_COND_V_MSG(!is_valid_animation_name(p_name), ERR_INVALID_PARAMETER, "Invalid animation name: '" + String(p_name) + "'.");
+	ERR_FAIL_COND_V_MSG(!is_valid_animation_name(p_name), ERR_INVALID_PARAMETER, vformat("Invalid animation name: '%s'.", String(p_name)));
+
 	ERR_FAIL_COND_V(p_animation.is_null(), ERR_INVALID_PARAMETER);
 
 	if (animations.has(p_name)) {
@@ -77,7 +78,7 @@ void AnimationLibrary::remove_animation(const StringName &p_name) {
 
 void AnimationLibrary::rename_animation(const StringName &p_name, const StringName &p_new_name) {
 	ERR_FAIL_COND_MSG(!animations.has(p_name), vformat("Animation not found: %s.", p_name));
-	ERR_FAIL_COND_MSG(!is_valid_animation_name(p_new_name), "Invalid animation name: '" + String(p_new_name) + "'.");
+	ERR_FAIL_COND_MSG(!is_valid_animation_name(p_new_name), vformat("Invalid animation name: '%s'.", p_new_name));
 	ERR_FAIL_COND_MSG(animations.has(p_new_name), vformat("Animation name \"%s\" already exists in library.", p_new_name));
 
 	animations.get(p_name)->disconnect_changed(callable_mp(this, &AnimationLibrary::_animation_changed));
